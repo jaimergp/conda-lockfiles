832154d53e9dcafddafe8341fac35ff23bd3d8b4
Jonathan Helmus <jjhelmus@gmail.com>
2025-05-07 16:25:14 -0500

    remove lockfiles-from-env subcommand

--- conda_lockfiles/plugin.py
+++ conda_lockfiles/plugin.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 from conda import plugins
 
 from . import cli
-from . import cli_from_env
 
 
 @plugins.hookimpl
@@ -13,10 +12,4 @@ def conda_subcommands():
         summary="Create new environments from different conda ecosystem lockfiles",
         action=cli.execute,
         configure_parser=cli.configure_parser,
-    )
-    yield plugins.CondaSubcommand(
-        name="lockfiles-from-env",
-        summary="Create a lockfile from an existing enviromment",
-        action=cli_from_env.execute,
-        configure_parser=cli_from_env.configure_parser,
     )
