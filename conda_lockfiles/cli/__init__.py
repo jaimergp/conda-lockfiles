989002f17d2416cb37120312af14f2d475019e77
Ken Odegard <kodegard@anaconda.com>
2025-05-07 16:40:09 -0500

    Add `--version`

--- conda_lockfiles/cli/__init__.py
+++ conda_lockfiles/cli/__init__.py
@@ -7,7 +7,17 @@ if TYPE_CHECKING:
 
 
 def configure_parser(parser: argparse.ArgumentParser) -> None:
+    from .. import APP_NAME, APP_VERSION
     from .main_create import configure_parser as configure_parser_create
+
+    # conda lockfiles --version
+    parser.add_argument(
+        "-V",
+        "--version",
+        action="version",
+        version=f"{APP_NAME} {APP_VERSION}",
+        help=f"Show the {APP_NAME} version number and exit.",
+    )
 
     subparsers = parser.add_subparsers(
         title="subcommand",
