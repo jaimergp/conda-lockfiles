039c8ce9259c4b8a0448f07539e39d0c8de5ed1b
Jonathan Helmus <jjhelmus@gmail.com>
2025-05-07 16:25:42 -0500

    smoke tests for subcommands

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,14 @@
 def test_cli_lockfiles_help(conda_cli):
-    conda_cli("lockfiles", "--help", raises=SystemExit)
+    _, _, exception = conda_cli("lockfiles", "--help", raises=SystemExit)
+    assert exception.value.code == 0
 
 
-def test_cli_lockfiles_from_env_help(conda_cli):
-    conda_cli("lockfiles", "--help", raises=SystemExit)
+def test_cli_lockfiles_create_help(conda_cli):
+    conda_cli("lockfiles", "create", "--help", raises=SystemExit)
+    _, _, exception = conda_cli("lockfiles", "create", "--help", raises=SystemExit)
+    assert exception.value.code == 0
+
+
+def test_cli_lockfiles_export_help(conda_cli):
+    _, _, exception = conda_cli("lockfiles", "export", "--help", raises=SystemExit)
+    assert exception.value.code == 0
