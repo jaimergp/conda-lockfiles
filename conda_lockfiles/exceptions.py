1f9de7742d9cdb61c55b7601820d1d117619d3b1
Jonathan Helmus <jjhelmus@gmail.com>
2025-05-07 15:53:21 -0500

    add ExportLockfileFormatNotSupported exception

--- conda_lockfiles/exceptions.py
+++ conda_lockfiles/exceptions.py
@@ -7,3 +7,9 @@ class LockfileFormatNotSupported(CondaError):
     def __init__(self, path: str):
         message = f"The specified file {path} is not supported."
         super().__init__(message)
+
+
+class ExportLockfileFormatNotSupported(CondaError):
+    def __init__(self, lockfile_format: str):
+        message = f"Exporting to lockfile format {lockfile_format} is not supported."
+        super().__init__(message)
