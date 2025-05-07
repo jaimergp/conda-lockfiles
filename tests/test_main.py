83e60974adf125f79c10e7b3293ede9e978cf1ba
Jonathan Helmus <jjhelmus@gmail.com>
2025-05-07 15:53:54 -0500

    add lockfiles-from-env smoketest

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,2 +1,6 @@
-def test_cli_help(conda_cli):
+def test_cli_lockfiles_help(conda_cli):
+    conda_cli("lockfiles", "--help", raises=SystemExit)
+
+
+def test_cli_lockfiles_from_env_help(conda_cli):
     conda_cli("lockfiles", "--help", raises=SystemExit)
