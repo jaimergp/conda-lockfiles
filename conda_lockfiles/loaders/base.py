379b44f57a1d02037e47ba61bfc4fc8b60afe6b1
Ken Odegard <kodegard@anaconda.com>
2025-05-08 02:23:50 -0600

    `conda.common.path.PathType`, `ABC`, and explicit returns (#6)
    * Use `conda.common.path.PathType`
    
    * Use abstract class
    
    * Explicit return
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- conda_lockfiles/loaders/base.py
+++ conda_lockfiles/loaders/base.py
@@ -1,30 +1,38 @@
 from __future__ import annotations
 
+from abc import ABC, abstractmethod
 from pathlib import Path
 from typing import TYPE_CHECKING
 
 from conda.base.context import context
 
 if TYPE_CHECKING:
-    from collections.abc import Iterable
+    from typing import Any
 
+    from conda.common.path import PathType
     from conda.models.records import PackageRecord
 
 
-class BaseLoader:
-    def __init__(self, path: str | Path):
+class BaseLoader(ABC):
+    def __init__(self, path: PathType):
         self.path = Path(path)
         self.data = self._load(path)
 
     @classmethod
-    def supports(cls, path: str | Path) -> bool:
+    @abstractmethod
+    def supports(cls, path: PathType) -> bool:
         raise NotImplementedError
 
+    @abstractmethod
+    def _load(self, path: PathType) -> dict[str, Any]:
+        raise NotImplementedError
+
+    @abstractmethod
     def to_conda_and_pypi(
         self,
         environment: str | None = None,
         platform: str = context.subdir,
-    ) -> tuple[Iterable[PackageRecord], Iterable[str]]:
+    ) -> tuple[tuple[PackageRecord, ...], tuple[str, ...]]:
         raise NotImplementedError
 
 
