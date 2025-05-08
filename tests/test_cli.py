da30c01274b378f0bec45043a9d3dce247d07938
Ken Odegard <kodegard@anaconda.com>
2025-05-08 14:31:49 -0500

    Update test_cli.py

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,17 +2,28 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from conda_lockfiles import APP_NAME, APP_VERSION
+
 if TYPE_CHECKING:
     from conda.testing.fixtures import CondaCLIFixture
 
 
 def test_help(conda_cli: CondaCLIFixture) -> None:
-    conda_cli("lockfiles", "--help", raises=SystemExit)
+    _, _, exception = conda_cli("lockfiles", "--help", raises=SystemExit)
+    assert exception.value.code == 0
 
 
 def test_version(conda_cli: CondaCLIFixture) -> None:
-    conda_cli("lockfiles", "--version", raises=SystemExit)
+    stdout, _, exception = conda_cli("lockfiles", "--version", raises=SystemExit)
+    assert f"{APP_NAME} {APP_VERSION}" == stdout.strip()
+    assert exception.value.code == 0
 
 
 def test_create_help(conda_cli: CondaCLIFixture) -> None:
-    conda_cli("lockfiles", "create", "--help", raises=SystemExit)
+    _, _, exception = conda_cli("lockfiles", "create", "--help", raises=SystemExit)
+    assert exception.value.code == 0
+
+
+def test_export_help(conda_cli: CondaCLIFixture) -> None:
+    _, _, exception = conda_cli("lockfiles", "export", "--help", raises=SystemExit)
+    assert exception.value.code == 0
