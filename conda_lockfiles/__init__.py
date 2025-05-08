a65ce0f2c5996683036e945b193cbcc1d42adaf7
Ken Odegard <kodegard@anaconda.com>
2025-05-08 02:24:56 -0600

    Add `--version` (#7)
    * Add `--version`
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- conda_lockfiles/__init__.py
+++ conda_lockfiles/__init__.py
@@ -2,6 +2,35 @@
 conda lockfiles: create conda environments from different lockfiles.
 """
 
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 from .create import create_environment_from_lockfile
 
+if TYPE_CHECKING:
+    from typing import Final
+
 __all__ = ["create_environment_from_lockfile"]
+
+#: Application name.
+APP_NAME: Final = "conda-lockfiles"
+
+try:
+    from ._version import __version__
+except ImportError:
+    # _version.py is only created after running `pip install`
+    try:
+        from setuptools_scm import get_version
+
+        __version__ = get_version(root="..", relative_to=__file__)
+    except (ImportError, OSError, LookupError):
+        # ImportError: setuptools_scm isn't installed
+        # OSError: git isn't installed
+        # LookupError: setuptools_scm unable to detect version
+        # conda-anaconda-tos follows SemVer, so the dev version is:
+        #     MJ.MN.MICRO.devN+gHASH[.dirty]
+        __version__ = "0.0.0.dev0+placeholder"
+
+#: Application version.
+APP_VERSION: Final = __version__
