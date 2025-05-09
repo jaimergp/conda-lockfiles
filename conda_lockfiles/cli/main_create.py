7fc2cabebd09006fc6f33ffd48ee6e2026eab63e
Ken Odegard <kodegard@anaconda.com>
2025-05-09 02:55:36 -0600

    Fix argparse help (#13)

--- conda_lockfiles/cli/main_create.py
+++ conda_lockfiles/cli/main_create.py
@@ -9,10 +9,14 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     import argparse
 
+HELP = "Create a conda environment from a lock file."
+
 
 def configure_parser(parser: argparse.ArgumentParser) -> None:
     from conda.base.context import context
     from conda.cli.helpers import add_parser_prefix
+
+    parser.description = HELP
 
     add_parser_prefix(parser, True)
     parser.add_argument("path", help="Path to pixi.lock file")
