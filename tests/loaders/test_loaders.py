fe3ca9d53f05bffabd0329faeaafb57a7d54a280
Jonathan Helmus <jjhelmus@gmail.com>
2025-06-03 15:09:52 -0500

    update test_loaders

--- tests/loaders/test_loaders.py
+++ tests/loaders/test_loaders.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
-from conda_lockfiles.loaders import LOADERS, BaseLoader, PixiLoader
+from conda_lockfiles.loaders import LOADERS, BaseLoader, CondaLockV1Loader, PixiLoader
 
 
 def test_loaders() -> None:
     assert LOADERS
     assert BaseLoader not in LOADERS
     assert PixiLoader in LOADERS
+    assert CondaLockV1Loader in LOADERS
