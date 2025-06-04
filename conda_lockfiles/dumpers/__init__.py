6d373fa83e3ff863ed36ec38a71d3ce15c1cad34
Jonathan Helmus <jjhelmus@gmail.com>
2025-06-04 14:44:01 -0500

    add support for exporting to explicit files

--- conda_lockfiles/dumpers/__init__.py
+++ conda_lockfiles/dumpers/__init__.py
@@ -1,9 +1,15 @@
 from __future__ import annotations
 
 from .conda_lock_v1 import export_to_conda_lock_v1
+from .explicit import export_to_explicit
 
-__all__ = ["export_to_conda_lock_v1", "LOCKFILE_FORMATS"]
+__all__ = [
+    "export_to_conda_lock_v1",
+    "export_to_explicit",
+    "LOCKFILE_FORMATS",
+]
 
 LOCKFILE_FORMATS: dict[str, callable] = {
     "conda-lock-v1": export_to_conda_lock_v1,
+    "explicit": export_to_explicit,
 }
---
+++ tests/dumpers/test_explicit.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from conda_lockfiles.dumpers import explicit
+
+from .. import SINGLE_PACKAGE_ENV
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+
+def test_export_to_explicit(tmp_path: Path) -> None:
+    lockfile_path = tmp_path / "explicit.txt"
+    explicit.export_to_explicit(str(SINGLE_PACKAGE_ENV), str(lockfile_path))
+    assert lockfile_path.exists()
+    data = lockfile_path.read_text()
+    assert (
+        "https://conda.anaconda.org/conda-forge/noarch/python_abi-3.13-7_cp313.conda"
+        in data
+    )
+    assert "0595134584589064f56e67d3de1d8fcbb673a972946bce25fb593fb092fdcd97" in data
+    assert "@EXPLICIT" in data
