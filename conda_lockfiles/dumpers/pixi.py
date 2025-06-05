6fb1c045ab11f1899bc0f31cb7b1bc0a5ecbc283
Jonathan Helmus <jjhelmus@gmail.com>
2025-06-05 16:15:38 -0500

    fix comments

--- conda_lockfiles/dumpers/pixi.py
+++ conda_lockfiles/dumpers/pixi.py
@@ -21,7 +21,7 @@ def _record_to_pixi_v6_package(record: PackageRecord) -> dict[str, Any]:
     # add relevent non-empty fields that rattler_lock includes in v6 lockfiles
     # https://github.com/conda/rattler/blob/rattler_lock-v0.23.5/crates/rattler_lock/src/parse/models/v6/conda_package_data.rs#L46
     fields = [
-        # channel, subdir, name, build and version can be decuded from the URL
+        # channel, subdir, name, build and version can be determined from the URL
         "sha256",
         "md5",
         "depends",
@@ -31,7 +31,7 @@ def _record_to_pixi_v6_package(record: PackageRecord) -> dict[str, Any]:
         "license",
         "license_family",
         "size",
-        # libmamba-conda-solver record the repodata timestamp,
+        # libmamba-conda-solver does not record the repodata timestamp,
         # do not include this field, see:
         # https://github.com/conda/conda-libmamba-solver/issues/673
         # "timestamp",
