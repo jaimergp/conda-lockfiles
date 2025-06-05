c2e77d85c729f5a7bd7afab59cfbc4cb44081021
Jonathan Helmus <jjhelmus@gmail.com>
2025-06-05 16:13:51 -0500

    expand import line for future addition

--- conda_lockfiles/dumpers/__init__.py
+++ conda_lockfiles/dumpers/__init__.py
@@ -3,7 +3,11 @@ from __future__ import annotations
 from .conda_lock_v1 import export_to_conda_lock_v1
 from .pixi import export_to_pixi_v6
 
-__all__ = ["export_to_conda_lock_v1", "export_to_pixi_v6", "LOCKFILE_FORMATS"]
+__all__ = [
+    "export_to_conda_lock_v1",
+    "export_to_pixi_v6",
+    "LOCKFILE_FORMATS",
+]
 
 LOCKFILE_FORMATS: dict[str, callable] = {
     "conda-lock-v1": export_to_conda_lock_v1,
