ff1b757a3de4a1d0fda6beb619ff90768a030f46
Jonathan J. Helmus <jjhelmus@gmail.com>
2025-06-05 15:03:25 -0400

    support for creating environments from explicit (.txt) files (#23)
    * add support for creating from explicit files
    
    * add explicit.txt file

--- tests/loaders/test_loaders.py
+++ tests/loaders/test_loaders.py
@@ -1,6 +1,12 @@
 from __future__ import annotations
 
-from conda_lockfiles.loaders import LOADERS, BaseLoader, CondaLockV1Loader, PixiLoader
+from conda_lockfiles.loaders import (
+    LOADERS,
+    BaseLoader,
+    CondaLockV1Loader,
+    ExplicitLoader,
+    PixiLoader,
+)
 
 
 def test_loaders() -> None:
@@ -8,3 +14,4 @@ def test_loaders() -> None:
     assert BaseLoader not in LOADERS
     assert PixiLoader in LOADERS
     assert CondaLockV1Loader in LOADERS
+    assert ExplicitLoader in LOADERS
